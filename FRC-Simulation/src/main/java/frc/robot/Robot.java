97935303958ea423fa5c921a2ab8e73954a06985
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-09-25 14:11:09 -0400

    Setting up A-kit for logging and learning more about it

--- FRC-Simulation/src/main/java/frc/robot/Robot.java
+++ FRC-Simulation/src/main/java/frc/robot/Robot.java
@@ -4,16 +4,49 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
 
   public Robot() {
+    Logger.recordMetadata("ProjectName", "FRC-Simulation"); // Set a metadata value
+
+    switch(Constants.CURRENT_MODE){
+
+      case REAL:
+    
+        Logger.addDataReceiver(new WPILOGWriter());
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+
+      case SIM:
+
+        Logger.addDataReceiver(new NT4Publisher());
+        break;
+
+      case REPLAY:
+
+        setUseTiming(false);
+        String logPath = LogFileUtil.findReplayLog();
+        Logger.setReplaySource(new WPILOGReader(logPath));
+        Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_replay")));
+        break;
+    }
+
+    Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
+
     m_robotContainer = new RobotContainer();
   }
 
