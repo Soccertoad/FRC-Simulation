62070912ed793dff06315cb1af7d0695ae06a72f
Ben Reinert <146287616+Soccertoad@users.noreply.github.com>
2025-10-25 14:49:59 -0400

    Maple Sim SetUp

--- src/main/java/frc/robot/Subsystems/Swerve/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/Subsystems/Swerve/CommandSwerveDrivetrain.java
@@ -22,11 +22,13 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
@@ -54,6 +56,9 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
   private final SwerveRequest.SysIdSwerveTranslation m_translationCharacterization = new SwerveRequest.SysIdSwerveTranslation();
   private final SwerveRequest.SysIdSwerveSteerGains m_steerCharacterization = new SwerveRequest.SysIdSwerveSteerGains();
   private final SwerveRequest.SysIdSwerveRotation m_rotationCharacterization = new SwerveRequest.SysIdSwerveRotation();
+
+  /* Maple Sim */
+  private MapleSimSwerveDrivetrain mapleSimSwerveDrivetrain = null;
 
   /*
    * SysId routine for characterizing translation. This is used to find PID gains
@@ -131,7 +136,10 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
   public CommandSwerveDrivetrain(
       SwerveDrivetrainConstants drivetrainConstants,
       SwerveModuleConstants<?, ?, ?>... modules) {
-    super(drivetrainConstants, modules);
+    super(
+      drivetrainConstants, 
+      MapleSimSwerveDrivetrain.regulateModuleConstantsForSimulation(modules)
+    );
     if (Utils.isSimulation()) {
       startSimThread();
     }
@@ -156,7 +164,11 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
       SwerveDrivetrainConstants drivetrainConstants,
       double odometryUpdateFrequency,
       SwerveModuleConstants<?, ?, ?>... modules) {
-    super(drivetrainConstants, odometryUpdateFrequency, modules);
+    super(
+      drivetrainConstants, 
+      odometryUpdateFrequency, 
+      MapleSimSwerveDrivetrain.regulateModuleConstantsForSimulation(modules)
+    );
     if (Utils.isSimulation()) {
       startSimThread();
     }
@@ -195,7 +207,13 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
       Matrix<N3, N1> odometryStandardDeviation,
       Matrix<N3, N1> visionStandardDeviation,
       SwerveModuleConstants<?, ?, ?>... modules) {
-    super(drivetrainConstants, odometryUpdateFrequency, odometryStandardDeviation, visionStandardDeviation, modules);
+    super(
+      drivetrainConstants, 
+      odometryUpdateFrequency, 
+      odometryStandardDeviation, 
+      visionStandardDeviation, 
+      MapleSimSwerveDrivetrain.regulateModuleConstantsForSimulation(modules)
+    );
     if (Utils.isSimulation()) {
       startSimThread();
     }
@@ -304,18 +322,35 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     }
   }
 
+  @SuppressWarnings("unchecked")
   private void startSimThread() {
-    m_lastSimTime = Utils.getCurrentTimeSeconds();
-
+    mapleSimSwerveDrivetrain = new MapleSimSwerveDrivetrain(
+      Seconds.of(kSimLoopPeriod),
+      // TODO: modify the following constants according to your robot
+      Pounds.of(115), // robot weight
+      Inches.of(32), // bumper length
+      Inches.of(28), // bumper width
+      DCMotor.getKrakenX60(1), // drive motor type
+      DCMotor.getFalcon500(1), // steer motor type
+      1.2, // wheel COF
+      getModuleLocations(),
+      getPigeon2(),
+      getModules(),
+      TunerConstants.FrontLeft,
+      TunerConstants.FrontRight,
+      TunerConstants.BackLeft,
+      TunerConstants.BackRight
+    );
     /* Run simulation at a faster rate so PID gains behave more reasonably */
-    m_simNotifier = new Notifier(() -> {
-      final double currentTime = Utils.getCurrentTimeSeconds();
-      double deltaTime = currentTime - m_lastSimTime;
-      m_lastSimTime = currentTime;
-
-      /* use the measured time delta, get battery voltage from WPILib */
-      updateSimState(deltaTime, RobotController.getBatteryVoltage());
-    });
+    m_simNotifier = new Notifier(mapleSimSwerveDrivetrain::update);
     m_simNotifier.startPeriodic(kSimLoopPeriod);
   }
+
+  @Override
+  public void resetPose(Pose2d pose) {
+    if (this.mapleSimSwerveDrivetrain != null)
+        mapleSimSwerveDrivetrain.mapleSimDrive.setSimulationWorldPose(pose);
+    Timer.delay(0.05); // Wait for simulation to update
+    super.resetPose(pose);
+}
 }
